a4afb3b4c612d005ae787641b1ed9d6002ba35bb
Rakhi <rakhipanwar819@gmail.com>
2025-10-31 20:08:54 +0530

    tuple and tuple-methods

---
+++ code/tupleMethods.py
@@ -0,0 +1,10 @@
+a=(2,6,49,8,99,8,5,0,32,77)
+print(a)
+
+print(a.count(8))
+
+print(a.index(8))
+
+print(a*2)
+
+print(32 in a)
---
+++ code/tuples.py
@@ -0,0 +1,8 @@
+# tuples are immutable
+a=(2,6,49,8,99,8,5,0,32,77)
+print(type(a))
+
+b=()#empty tuple
+
+c=(5,) #tuple with only one element needs a comma
+print(c)
