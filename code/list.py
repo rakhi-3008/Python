c67f71f8fdd8c4fef84f39885fb5504b7a642666
Rakhi <rakhipanwar819@gmail.com>
2025-10-31 20:05:07 +0530

    list and list-methods

---
+++ code/list.py
@@ -0,0 +1,5 @@
+# lists are mutable
+fruits = ["apple","banana","litchi",7,True,"rakhi","sourav"]
+print(fruits[2])
+fruits[2]="cherry"
+print(fruits[2:5])
---
+++ code/listMethods.py
@@ -0,0 +1,21 @@
+fruits = ["apple","banana","litchi",7,True,"rakhi","sourav"]
+
+fruits.append("hello")
+print(fruits)
+
+fruits.reverse()
+print(fruits)
+
+numlist=[4,7,89,46,4,8,6,545,2]
+numlist.sort()
+print(numlist)
+
+numlist.insert(4,18)
+print(numlist)
+
+value = numlist.pop(6)
+print(value)
+print(numlist)
+
+numlist.remove(545)
+print(numlist)
