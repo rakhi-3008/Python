8e5ef44e32cc3c15bc8618e88e87618b9c4d349d
Rakhi <rakhipanwar819@gmail.com>
2026-02-28 23:55:42 +0530

    break statement

---
+++ code/break.py
@@ -0,0 +1,18 @@
+for i in range(1,11):
+    if(i == 3):
+        break
+    print(i)
+
+for i in range(1,11):
+    if(i==3):
+        continue
+    print(i)
+
+#  pass statement
+for j in range(10):
+    pass    # pass means do nothing
+
+m=0
+while(m<10):
+    print(m)
+
